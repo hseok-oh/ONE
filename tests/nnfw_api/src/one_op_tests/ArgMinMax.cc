97e7f795419c47abe8bcd694197bd88ca86c2aa3
Hyeongseok Oh <hseok82.oh@samsung.com>
2021-08-30 16:19:27 +0900

    [onert/test] Change ArgMinMax param test
    This commit changes ArgMinMax parameter test to fixture test.
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- tests/nnfw_api/src/one_op_tests/ArgMinMax.cc
+++ tests/nnfw_api/src/one_op_tests/ArgMinMax.cc
@@ -18,51 +18,22 @@
 
 #include <memory>
 
-struct ArgMinMaxVariationParam
-{
-  TestCaseData tcd;
-  bool is_argmax = true;
-  circle::TensorType input_type = circle::TensorType::TensorType_FLOAT32;
-  float scale = 0.0f;
-  int64_t zero_point = 0;
-};
-
-class ArgMinMaxVariation : public GenModelTest,
-                           public ::testing::WithParamInterface<ArgMinMaxVariationParam>
-{
-};
-
 // Input shape: {1, 2, 2, 1}
 // Reduce axis: 1
 // Output shape: {1, 2, 1}
 // Output type: Int32
-TEST_P(ArgMinMaxVariation, Test)
+TEST_F(GenModelTest, OneOp_ArgMinMax)
 {
-  auto &param = GetParam();
+  struct ArgMinMaxVariationParam
+  {
+    TestCaseData tcd;
+    bool is_argmax = true;
+    circle::TensorType input_type = circle::TensorType::TensorType_FLOAT32;
+    float scale = 0.0f;
+    int64_t zero_point = 0;
+  };
 
-  CircleGen cgen;
-  const auto output_type = circle::TensorType::TensorType_INT32;
-  std::vector<int32_t> axis_data{1};
-  uint32_t axis_buf = cgen.addBuffer(axis_data);
-  int axis = cgen.addTensor({{1}, circle::TensorType::TensorType_INT32, axis_buf});
-  int in = cgen.addTensor({{1, 2, 2, 1}, param.input_type}, param.scale, param.zero_point);
-  int out = cgen.addTensor({{1, 2, 1}, output_type});
-  param.is_argmax ? cgen.addOperatorArgMax({{in, axis}, {out}}, output_type)
-                  : cgen.addOperatorArgMin({{in, axis}, {out}}, output_type);
-  cgen.setInputsAndOutputs({in}, {out});
-
-  _context = std::make_unique<GenModelTestContext>(cgen.finish());
-  _context->addTestCase(param.tcd);
-  _context->setBackends({"acl_cl", "acl_neon", "cpu"});
-
-  SUCCEED();
-}
-
-// Test with different input type and value
-INSTANTIATE_TEST_CASE_P(
-  GenModelTest, ArgMinMaxVariation,
-  ::testing::Values(
-    // ArgMax, float input
+  static const std::vector<ArgMinMaxVariationParam> examples{
     ArgMinMaxVariationParam{TestCaseData{}.addInput<float>({1, 4, 2, 3}).addOutput<int32_t>({1, 0}),
                             true},
     // ArgMax, int32 input
@@ -91,7 +62,28 @@ INSTANTIATE_TEST_CASE_P(
     // ArgMin, int8 input
     ArgMinMaxVariationParam{
       TestCaseData{}.addInput<int8_t>({1, 4, 2, 3}).addOutput<int32_t>({0, 1}), false,
-      circle::TensorType::TensorType_INT8, 1.0, 1}));
+      circle::TensorType::TensorType_INT8, 1.0, 1}};
+
+  for (auto example : examples)
+  {
+    CircleGen cgen;
+    const auto output_type = circle::TensorType::TensorType_INT32;
+    std::vector<int32_t> axis_data{1};
+    uint32_t axis_buf = cgen.addBuffer(axis_data);
+    int axis = cgen.addTensor({{1}, circle::TensorType::TensorType_INT32, axis_buf});
+    int in = cgen.addTensor({{1, 2, 2, 1}, example.input_type}, example.scale, example.zero_point);
+    int out = cgen.addTensor({{1, 2, 1}, output_type});
+    example.is_argmax ? cgen.addOperatorArgMax({{in, axis}, {out}}, output_type)
+                      : cgen.addOperatorArgMin({{in, axis}, {out}}, output_type);
+    cgen.setInputsAndOutputs({in}, {out});
+
+    _context = std::make_unique<GenModelTestContext>(cgen.finish());
+    _context->addTestCase(example.tcd);
+    _context->setBackends({"acl_cl", "acl_neon", "cpu"});
+  }
+
+  SUCCEED();
+}
 
 TEST_F(GenModelTest, OneOp_ArgMax_Int64_AxisToConst)
 {
