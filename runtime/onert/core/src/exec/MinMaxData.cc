f7149cb6fca230149d208aeea598e140e93a715a
Hyeongseok Oh <hseok82.oh@samsung.com>
2024-06-26 15:28:34 +0900

    Fix analyzer defects

--- runtime/onert/core/src/exec/MinMaxData.cc
+++ runtime/onert/core/src/exec/MinMaxData.cc
@@ -18,6 +18,29 @@
 
 #include <iostream>
 
+namespace
+{
+
+void writeMMFile(const void *ptr, size_t size, size_t count, FILE *fp, const std::string &err_msg)
+{
+  if (std::fwrite(ptr, size, count, fp) != count)
+  {
+    std::fclose(fp);
+    throw std::runtime_error(err_msg);
+  }
+}
+
+void seekMMFile(FILE *fp, int64_t offset, int whence, const std::string &err_msg)
+{
+  if (std::fseek(fp, offset, whence) != 0)
+  {
+    std::fclose(fp);
+    throw std::runtime_error(err_msg);
+  }
+}
+
+} // namespace
+
 namespace onert
 {
 namespace exec
@@ -45,13 +68,15 @@ void RawMinMaxDumper::dump(const exec::IOMinMaxMap &input_minmax,
       throw std::runtime_error{"RawMinMaxDumper: Failed to open minmax file " + _filename};
 
     // Write magic code and version
-    std::fwrite(&MAGIC_CODE, sizeof(uint32_t), 1, file);
-    std::fwrite(&VERSION, sizeof(uint32_t), 1, file);
+    writeMMFile(&MAGIC_CODE, sizeof(uint32_t), 1, file,
+                "RawMinMaxDumper: Failed to write magic code");
+    writeMMFile(&VERSION, sizeof(uint32_t), 1, file, "RawMinMaxDumper: Failed to write version");
   }
   else
   {
     // Check magic code and version
-    std::fseek(file, 0, SEEK_SET);
+    seekMMFile(file, 0, SEEK_SET, "RawMinMaxDumper: Failed to seek file start position");
+    ;
     uint32_t read_magic_code = 0;
     uint32_t read_version = 0;
     bool rewrite = true;
@@ -69,8 +94,9 @@ void RawMinMaxDumper::dump(const exec::IOMinMaxMap &input_minmax,
         throw std::runtime_error{"RawMinMaxDumper: Failed to rewrite minmax file " + _filename};
 
       // Write magic code and version
-      std::fwrite(&MAGIC_CODE, sizeof(uint32_t), 1, file);
-      std::fwrite(&VERSION, sizeof(uint32_t), 1, file);
+      writeMMFile(&MAGIC_CODE, sizeof(uint32_t), 1, file,
+                  "RawMinMaxDumper: Failed to write magic code");
+      writeMMFile(&VERSION, sizeof(uint32_t), 1, file, "RawMinMaxDumper: Failed to write version");
     }
   }
 
@@ -83,18 +109,20 @@ void RawMinMaxDumper::dump(const exec::IOMinMaxMap &input_minmax,
   // TODO Verify file size
 
   // Overwrite run count
-  std::fseek(file, sizeof(MAGIC_CODE) + sizeof(VERSION), SEEK_SET);
-  std::fwrite(&runs, sizeof(uint32_t), 1, file);
+  seekMMFile(file, sizeof(MAGIC_CODE) + sizeof(VERSION), SEEK_SET,
+             "RawMinMaxDumper: Failed to seek to header");
+  writeMMFile(&runs, sizeof(uint32_t), 1, file, "RawMinMaxDumper: Failed to write run count");
 
   // Go to end of file to append new data
-  std::fseek(file, 0, SEEK_END);
+  seekMMFile(file, 0, SEEK_END, "RawMinMaxDumper: Failed to seek to end of file");
 
   uint32_t input_count = input_minmax.size();
   uint32_t op_count = op_minmax.size();
 
   // Write op_count and input_count
-  std::fwrite(&op_count, sizeof(uint32_t), 1, file);
-  std::fwrite(&input_count, sizeof(uint32_t), 1, file);
+  writeMMFile(&op_count, sizeof(uint32_t), 1, file, "RawMinMaxDumper: Failed to write op_count");
+  writeMMFile(&input_count, sizeof(uint32_t), 1, file,
+              "RawMinMaxDumper: Failed to write input_count");
 
   // For each op
   for (auto &&elem : op_minmax)
@@ -104,12 +132,15 @@ void RawMinMaxDumper::dump(const exec::IOMinMaxMap &input_minmax,
     const uint32_t op_idx = elem.first.second.value();
 
     // Write model/subg/op index
-    std::fwrite(&model_idx, sizeof(uint32_t), 1, file);
-    std::fwrite(&subg_idx, sizeof(uint32_t), 1, file);
-    std::fwrite(&op_idx, sizeof(uint32_t), 1, file);
+    writeMMFile(&model_idx, sizeof(uint32_t), 1, file,
+                "RawMinMaxDumper: Failed to write OP model index");
+    writeMMFile(&subg_idx, sizeof(uint32_t), 1, file,
+                "RawMinMaxDumper: Failed to write OP subg index");
+    writeMMFile(&op_idx, sizeof(uint32_t), 1, file, "RawMinMaxDumper: Failed to write OP index");
 
     // Write min/max
-    std::fwrite(elem.second.data, sizeof(float), 2, file);
+    writeMMFile(elem.second.data, sizeof(float), 2, file,
+                "RawMinMaxDumper: Failed to write OP min/max");
   }
 
   // For each input
@@ -120,12 +151,16 @@ void RawMinMaxDumper::dump(const exec::IOMinMaxMap &input_minmax,
     const uint32_t input_idx = elem.first.second.value();
 
     // Write model/subg/input index
-    std::fwrite(&model_idx, sizeof(uint32_t), 1, file);
-    std::fwrite(&subg_idx, sizeof(uint32_t), 1, file);
-    std::fwrite(&input_idx, sizeof(uint32_t), 1, file);
+    writeMMFile(&model_idx, sizeof(uint32_t), 1, file,
+                "RawMinMaxDumper: Failed to write input model index");
+    writeMMFile(&subg_idx, sizeof(uint32_t), 1, file,
+                "RawMinMaxDumper: Failed to write input subg index");
+    writeMMFile(&input_idx, sizeof(uint32_t), 1, file,
+                "RawMinMaxDumper: Failed to write input index");
 
     // Write min/max
-    std::fwrite(elem.second.data, sizeof(float), 2, file);
+    writeMMFile(elem.second.data, sizeof(float), 2, file,
+                "RawMinMaxDumper: Failed to write min/max");
   }
 
   std::fclose(file);
